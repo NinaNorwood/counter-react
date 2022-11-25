981915ff6e922096119ebd6d3108feaed2189a8e
ninamichalski <ninamichalski@gmx.net>
2022-11-25 15:14:23 +0100

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+ // const [data, setData] = useState("");
+  //  <input onChange={(event) => setData(event.target.value)}></input>
+   // <h1>{data}</h1>
+
+  const onButtonPressPlus = () => {
+    setCount(count+1);
+}
+
+    const onButtonPressMinus = () => {
+     setCount(count-1);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="more" onClick={onButtonPressPlus}>Click for more</button>
+        <h1>{count}</h1>
+      <button className="less" onClick={onButtonPressMinus}>Click for less</button>
     </div>
   );
 }
